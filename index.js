cae2281750a3d4f71fd91e3a1e51ec588a0d42f5
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-06-29 19:21:11 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word = word.toLowerCase();
+
+  
+  
+  // find the length of the string
+  let len = word.length;
+
+
+
+  for (let i = 0; i < len/2; i++) {
+    if (word[i] !== word[len - 1 -i]) {
+      return false
+    }
+  }
+  return true
 }
 
 /* 
-  Add your pseudocode here
+  function isPalindrome takes in word
+    convert word to lower case
+    
+    len = length of word
+    
+    for i from 0 to len/2
+    if character at position i in word is not equal to character at position (len-1-i) in word
+    return false
+    
+    return true
+end function
 */
 
 /*
-  Add written explanation of your solution here
+  Explanation of your solution here
+
+It converts the input string word to lowercase to ensure that the function is case-insensitive.
+
+
+It finds the length of the word string.
+
+It iterates through the first half of the word string. For each character at position i, it checks if it is equal to the character at the mirrored position from the end (len - 1 - i). If it finds any pair of characters that do not match, it immediately returns false, indicating that the string is not a palindrome.
+
+If the loop completes without finding any mismatched characters, the function returns true, indicating that the string is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
